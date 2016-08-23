c7dd1468ba6c5dd516fdda13d87a7f580a24761d
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-08-23 17:21:06 +0300

    Added implementation

--- src/main/java/com/clouway/MyClass.java
+++ src/main/java/com/clouway/MyClass.java
@@ -1,7 +1,22 @@
 package com.clouway;
 
+import org.apache.log4j.Logger;
+
+import java.util.Scanner;
+
 /**
  * @author Petar Nedelchev <peter.krasimirov@gmail.com>
  */
 public class MyClass {
+
+    private static Logger logger = Logger.getLogger(MyClass.class);
+
+    public static void main(String[] args) {
+        DirectoryBrowser browser = new DirectoryBrowser();
+        Scanner scanner = new Scanner(System.in);
+        String path;
+        path = scanner.nextLine();
+        logger.info("Directory browser was called with path:" + path +
+                "\n\t return:" + browser.listContent(path));
+    }
 }
